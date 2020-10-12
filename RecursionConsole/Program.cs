eab5ac140b291b4b454cd58c9f5ced0762acd55b
ThansiJamal <thansijamal18@gmail.com>
2020-10-12 10:05:05 -0700

    Edit for commit

--- RecursionConsole/Program.cs
+++ RecursionConsole/Program.cs
@@ -1,43 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace RecursionConsole
 {
     class Program
     {
-
-        static void PrintNumber(int i)
-        {
-            if (i > 10)
-                return;
-            Console.WriteLine(i + " ");
-            PrintNumber(i + 1);
-
-        }
+        static int i = 1;
         static void Main(string[] args)
         {
-            PrintNumber(1);
+            Add();
             Console.ReadKey();
         }
-        //static void PrintNumber(int i)
-        //{
-        //    if (i > 0)
-        //    {
-        //        PrintNumber(i - 1);
-        //        Console.WriteLine(i + " ");
-        //    } 
-        //        return;
-
-
-        //}
-        //static void Main(string[] args)
-        //{
-        //    PrintNumber(10);
-        //    Console.ReadKey();
-        //}
-
+       public static void Add()
+        {
+            Thread.Sleep(500);
+            Console.WriteLine(i.ToString());
+            i++;
+            if(i==11)
+            {
+                return;
+            }
+            Add();
+        }
+        
+        
     }
 }
